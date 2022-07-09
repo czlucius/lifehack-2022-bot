b1e4c022ef861d1fef18c2bec5b9a957d6878748
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-07-09 15:55:36 +0800

    Update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 import discord
 import os  # default module
 
@@ -11,9 +13,27 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
+@bot.slash_command(name="ping", description="Ping the bot, and get WebSocket latency")
 async def hello(ctx):
-    await ctx.respond("Hey!")
+
+    latency_ms = get_latency_ms(ctx.bot)
+    embed = discord.Embed(
+        title="Pong!",
+        description=f"Latency: {latency_ms} ms"
+    )
+    logging.info(f"/ping: latency is {latency_ms} ms")
+
+    await ctx.respond(embed=embed)
+
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Volunteer Matching Bot, type /help"))
+
+
+@bot.slash_command(name="view", description="View possible volunteering opportunities")
+@discord.option("category", "Enter category of volunteer opportunity you'd like to participate in")
+async def view(ctx, category):
+    pass
+
+async def
 
 
 bot.run(TOKEN)  # run the bot with the token
